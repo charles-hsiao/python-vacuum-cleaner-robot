42e97fb694e7abea9e25b7460dbbcdec06c36b78
Charles Hsiao <charles_hsiao@maicoin.com>
2018-10-11 17:59:01 +0800

    Add robot class

---
+++ robot.py
@@ -0,0 +1,74 @@
+from utils import sin, cos
+
+
+class Robot(object):
+
+    def __init__(self, matrix, start_position, start_direction):
+        self.matrix = matrix
+        self.current_position = {'x': start_position['x'], 'y': start_position['y']}
+        self.current_direction = start_direction
+        self.__visited_position = {str(start_position['x']) + '_' + str(start_position['y']): 1}
+        self.move_count = 0
+        self.turn_count = 0
+        self.loggable = False
+        self.path_history = []
+
+    def turn_left(self):
+        """turn 90 degree counter-clockwise"""
+        self.current_direction = (self.current_direction + 1) % 4
+        self.turn_count += 1
+        return self
+
+    def turn_right(self):
+        """turn 90 degree clockwise"""
+        self.current_direction = (self.current_direction + 3) % 4
+        self.turn_count += 1
+        return self
+
+    def move(self):
+        """move ahead"""
+        next_pos_x = self.current_position['x'] + cos(self.current_direction)
+        next_pos_y = self.current_position['y'] - sin(self.current_direction)
+        if not self.__can_move(next_pos_x, next_pos_y):
+            self.__visited_position[str(next_pos_x) + "_" + str(next_pos_y)] = -1
+            return False
+        self.move_count += 1
+        self.current_position['x'] = next_pos_x
+        self.current_position['y'] = next_pos_y
+        self.__visited_position[str(next_pos_x) + "_" + str(next_pos_y)] = 1
+        #print("[x,y]" + str(self.current_position['x']) + "," + str(self.current_position['y']))
+        self.path_history.append([self.current_position['x'], self.current_position['y']])
+        if self.loggable:
+            self.log()
+        return True
+
+    def __can_move(self, next_pos_x, next_pos_y):
+        if next_pos_x < 0 or next_pos_y < 0:
+            return False
+        if next_pos_y >= len(self.matrix):
+            return False
+        if next_pos_x >= len(self.matrix[0]):
+            return False
+        return self.matrix[next_pos_y][next_pos_x] == 0
+
+    def log(self):
+        for i in range(len(self.matrix)):
+            text = ""
+            for j in range(len(self.matrix[i])):
+                if i == self.current_position['y'] and j == self.current_position['x']:
+                    if self.current_direction == 0:
+                        text += '>'
+                    elif self.current_direction == 1:
+                        text += '^'
+                    elif self.current_direction == 2:
+                        text += '<'
+                    else:
+                        text += 'v'
+                elif self.__visited_position.get(str(j) + "_" + str(i), None) == 1:
+                    text += '*'
+                elif self.matrix[i][j] == 0:
+                    text += '.'
+                else:
+                    text += '|'
+            print(text)
+        print('')
---
+++ utils.py
@@ -0,0 +1,87 @@
+def cos(direction):
+    """naive implementation of cos"""
+    return int(abs(2 - direction) - 1)
+
+
+def sin(direction):
+    """naive implementation of cos"""
+    return int(1 - abs(direction - 1))
+
+
+def print_observed_map(sweeper):
+    min_y = min(sweeper.observed_map)
+    min_dict_x = min(sweeper.observed_map, key=lambda x: min(sweeper.observed_map[x]))
+    min_x = min(sweeper.observed_map[min_dict_x])
+    max_y = max(sweeper.observed_map)
+    max_dict_x = max(sweeper.observed_map, key=lambda x: max(sweeper.observed_map[x]))
+    max_x = max(sweeper.observed_map[max_dict_x])
+
+    for i in range(min_y, max_y + 1):
+        text = ""
+        for j in range(min_x, max_x + 1):
+            item = sweeper.observed_map[i].get(j, None)
+            if sweeper.current_position['x'] == j \
+                    and sweeper.current_position['y'] == i:
+                text += 'o'
+            elif not item:
+                text += ' '
+            elif item == 1:
+                text += '*'
+            else:
+                text += '|'
+        print(text)
+    print('')
+
+def bfs(start_position, start_direction, finish_check_fn, adjacent_check_fn, spiral):
+    # this is just simple BFS implementation
+    checked = {}
+    queue = []
+    queue.append({'x': start_position['x'], 'y': start_position['y'], 'direction': None, 'parent': None})
+
+    while queue:
+        current = queue.pop(0)
+        if current['direction'] is not None:
+            start_direction = current['direction']
+        finished = finish_check_fn(current)
+        if finished:
+            path = []
+            while current['parent']:
+                path.append(current['direction'])
+                current = current['parent']
+            return path
+        for node in adjacent(current, start_direction, spiral):
+            key = str(node['x']) + '_' + str(node['y'])
+            if not checked.get(key, None) \
+                    and adjacent_check_fn(node):
+                checked[key] = 1
+                queue.append(node)
+
+def adjacent(current, start_direction, spiral):
+    if spiral:
+        if start_direction == 0:
+            return [
+                {'x': current['x'], 'y': current['y'] - 1, 'direction': 1, 'parent': current},
+                {'x': current['x'] + 1, 'y': current['y'], 'direction': 0, 'parent': current},
+                {'x': current['x'], 'y': current['y'] + 1, 'direction': 3, 'parent': current},
+                {'x': current['x'] - 1, 'y': current['y'], 'direction': 2, 'parent': current}
+            ]
+        if start_direction == 1:
+            return [
+                {'x': current['x'] - 1, 'y': current['y'], 'direction': 2, 'parent': current},
+                {'x': current['x'], 'y': current['y'] - 1, 'direction': 1, 'parent': current},
+                {'x': current['x'] + 1, 'y': current['y'], 'direction': 0, 'parent': current},
+                {'x': current['x'], 'y': current['y'] + 1, 'direction': 3, 'parent': current}
+            ]
+        if start_direction == 2:
+            return [
+                {'x': current['x'], 'y': current['y'] + 1, 'direction': 3, 'parent': current},
+                {'x': current['x'] - 1, 'y': current['y'], 'direction': 2, 'parent': current},
+                {'x': current['x'], 'y': current['y'] - 1, 'direction': 1, 'parent': current},
+                {'x': current['x'] + 1, 'y': current['y'], 'direction': 0, 'parent': current}
+            ]
+    return [
+        {'x': current['x'] + 1, 'y': current['y'], 'direction': 0, 'parent': current},
+        {'x': current['x'], 'y': current['y'] + 1, 'direction': 3, 'parent': current},
+        {'x': current['x'] - 1, 'y': current['y'], 'direction': 2, 'parent': current},
+        {'x': current['x'], 'y': current['y'] - 1, 'direction': 1, 'parent': current}
+    ]
