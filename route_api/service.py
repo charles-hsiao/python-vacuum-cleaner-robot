c423c3b2a50c3369644dd86301b5497451e66f29
Charles Hsiao <charles_hsiao@maicoin.com>
2018-11-23 18:01:55 +0800

    Update as API return

--- route_api/service.py
+++ route_api/service.py
@@ -5,4 +5,8 @@ def handler(event, context):
     # Your code goes here!
     e = event.get('e')
     pi = event.get('pi')
-    return e + pi
+    return {
+        "statusCode": 200,
+        "headers": { "Content-Type": "application/json"},
+        "body": e + pi
+    }
