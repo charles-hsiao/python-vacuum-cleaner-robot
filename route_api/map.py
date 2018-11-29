4671adda7c4252fbea29c785a18d257f13ec81a3
Charles Hsiao <charles_hsiao@maicoin.com>
2018-11-29 15:40:36 +0800

    Add path attribute by different floor type

--- route_api/map.py
+++ route_api/map.py
@@ -3,10 +3,7 @@
 # Transfer map to algorithm-understanding matrix
 # Algorithm-understanding: 0=Able to pass; 1=Obstacle
 def map_matrix_transfer(matrix):
-    print(matrix)
     map_matrix = []
-    #x = 0
-    #y = 0
     for row in matrix:
         new_row = []
         for column in row:
@@ -22,3 +19,62 @@ def map_matrix_transfer(matrix):
 # m = [[0, -1, 0], [0, 0, 0], [0, 0, -1]]
 # map_matrix = map_matrix_transfer(m)
 # print(map_matrix)
+
+
+# Floor Definition
+# | Floor Type       | Code | Able to Cross | Clean Mode(Code)   | Clean Strength  |
+# | ---------------- | ---- | ------------- | ------------------ | --------------- |
+# | Empty            | -2   | X             | - (0)              | 0               |
+# | Obstacle         | -1   | X             | - (0)              | 0               |
+# | Normal Floor     | 0    | O             | Vacuum Cleaner (1) | 1               |
+# | Wet Floor        | 1    | O             | Mop (2)            | 0               |
+# | Carpet           | 2    | O             | Vacuum Cleaner (1) | 3               |
+# | Dirty Floor      | 3    | O             | Vacuum Cleaner (1) | 2               |
+# | Floor with Trash | 4    | O             | Broom (3)          | 0               |
+
+# Clean Mode: 0=None; 1=Vacuum Cleaner; 2=Mop; 3=Broom
+# Clean Strength(Only on Vacuum Cleaner mode): 0=None; 1=Week; 2=Medium; 3=Strong
+
+
+def path_append_attributes(matrix, path):
+    new_path = []
+    for spot in path:
+        x = spot[0]
+        y = spot[1]
+        floor_type = matrix[y][x]
+
+        if floor_type == 0:  # Normal Floor
+            clean_mode = 1
+            strength = 1
+        elif floor_type == 1:  # Wet Floor
+            clean_mode = 2
+            strength = 0
+        elif floor_type == 2:  # Carpet
+            clean_mode = 1
+            strength = 3
+        elif floor_type == 3:  # Dirty Floor
+            clean_mode = 1
+            strength = 2
+        elif floor_type == 4:  # Floor with Trash
+            clean_mode = 3
+            strength = 0
+        elif floor_type == -1:  # Empty (should not happen)
+            clean_mode = 0
+            strength = 0
+        elif floor_type == -2:  # Obstacle (should not happen)
+            clean_mode = 0
+            strength = 0
+        else:  # (should not happen)
+            clean_mode = 0
+            strength = 0
+
+        new_spot = [x, y, clean_mode, strength]
+        new_path.append(new_spot)
+
+    return new_path
+
+
+# matrix = [[4, -2, 0], [0, 1, 2], [3, 0, -1]]
+# path_history = [[0, 1], [0, 2], [1, 2], [1, 1], [2, 1], [2, 0], [2, 1], [1, 1], [1, 2], [0, 2], [0, 1], [0, 0]]
+# path_with_attributes = path_append_attributes(matrix, path_history)
+# print(path_with_attributes)
